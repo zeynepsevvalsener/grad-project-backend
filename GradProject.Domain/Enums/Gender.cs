064de6ffefc8cd0bf99bbcb0eea5cc3c6d2b8576
emresner <emresenery@gmail.com>
2025-12-14 23:19:07 +0300

    GRAD-57 Users & Profiles tables

---
+++ GradProject.Domain/Enums/Gender.cs
@@ -0,0 +1,9 @@
+﻿namespace GradProject.Domain.Enums
+{
+    public enum Gender
+    {
+        Unknown = 0,
+        Male = 1,
+        Female = 2
+    }
+}
---
+++ GradProject.Domain/Enums/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace GradProject.Domain.Enums
+{
+    public enum UserRole
+    {
+        User = 0,
+        Admin = 1
+    }
+}
