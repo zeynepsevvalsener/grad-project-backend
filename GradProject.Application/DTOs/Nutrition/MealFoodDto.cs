075173e3b7574c84df6dcffb1babb5c744857626
Utku.G <mehmetutku.g@hotmail.com>
2026-02-27 21:36:38 +0300

    GRAD-132 Extend NLP Engine for Turkish Parsing & Mixed Input Support

--- GradProject.Application/DTOs/Nutrition/MealFoodDto.cs
+++ GradProject.Application/DTOs/Nutrition/MealFoodDto.cs
@@ -2,9 +2,11 @@ namespace GradProject.Application.DTOs.Nutrition
 {
     public class MealFoodDto
     {
+        public int Id { get; set; }
         public int FoodId { get; set; }
+        public string FoodName { get; set; } = null!;
         public decimal Quantity { get; set; }
         public string Unit { get; set; } = null!;
+        public decimal PortionG { get; set; }
     }
 }
-
